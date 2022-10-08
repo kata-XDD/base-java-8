e465384d8992d74148c1afa4906a0bc262865a5d
Leddzip <leddzip@gmail.com>
2022-10-09 00:51:23 +0200

    first setup

---
+++ src/main/java/fr/leddzip/kata/xdd/base/java8xdd/Dummy.java
@@ -0,0 +1,9 @@
+package fr.leddzip.kata.xdd.base.java8xdd;
+
+public class Dummy {
+
+    public void dummify() throws IllegalAccessException {
+        throw new IllegalAccessException("Don-t call it, delete it");
+    }
+
+}
---
+++ src/test/java/fr/leddzip/kata/xdd/base/java8xdd/DummyTest.java
@@ -0,0 +1,13 @@
+package fr.leddzip.kata.xdd.base.java8xdd;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() throws IllegalAccessException {
+        throw new IllegalAccessException("Don't call it, delete it");
+    }
+
+}
